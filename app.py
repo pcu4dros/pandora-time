2d9d5d9ffe85fe175770ee55d0c79134c681e266
pcu4dros <pemcda@gmail.com>
2017-06-11 22:34:41 -0500

    Delete .pyc files and add to gitignore
    Add gitignore and delete .pyc files

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources import TaskAPI
 from flask_restful_swagger import swagger
 
 app = Flask(__name__, static_url_path="")
-api = swagger.docs(Api(app), apiVersion='0.1', description='task')
+api = swagger.docs(Api(app), apiVersion='0.1', description='task' )
 
 api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
 api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
